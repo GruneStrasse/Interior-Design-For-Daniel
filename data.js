2600f21ab9d0c059bfb8c00ab13e1578431d1aac
adamhayek2002 <116099346+GruneStrasse@users.noreply.github.com>
2022-12-06 13:54:33 +0200

    initial commit

---
+++ data.js
@@ -0,0 +1,199 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bedroom-1",
+      "name": "Bedroom 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8833964634667808,
+          "pitch": -0.06300094076452645,
+          "rotation": 0,
+          "target": "4-living-room"
+        },
+        {
+          "yaw": 1.8755548948204783,
+          "pitch": -0.04776659745844469,
+          "rotation": 0,
+          "target": "1-bathroom-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bathroom-1",
+      "name": "Bathroom 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.912350277247125,
+          "pitch": -0.0610879072729098,
+          "rotation": 0,
+          "target": "0-bedroom-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bathroom-2",
+      "name": "Bathroom 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1498672968525696,
+          "pitch": 0.010978010887946255,
+          "rotation": 0,
+          "target": "3-bedroom-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bedroom-2",
+      "name": "Bedroom 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9239798145620917,
+          "pitch": -0.006230842565969397,
+          "rotation": 0,
+          "target": "2-bathroom-2"
+        },
+        {
+          "yaw": 1.42496791061029,
+          "pitch": 0.055420407838733254,
+          "rotation": 0,
+          "target": "4-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-living-room",
+      "name": "Living room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8991878465860594,
+          "pitch": -0.06322332709763501,
+          "rotation": 0,
+          "target": "3-bedroom-2"
+        },
+        {
+          "yaw": 1.897127440862243,
+          "pitch": -0.02367927239196277,
+          "rotation": 0,
+          "target": "0-bedroom-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
